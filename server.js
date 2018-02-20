8a91defc0e77d30725a6f3975ec3a7d851a7bed7
Brandon Brown <Brandon.Brown@metova.com>
2018-02-20 13:52:56 -0600

    initialize weather app

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('upperCaseText', (text) => {
+  return text.toUpperCase();
+});
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((request, response, next) => {
+
+  next();
+});
+
+app.use((request, response, next) => {
+  response.render('maintenance.hbs');
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
